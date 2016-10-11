a36eddbaa75fac8a2d8d550a1d2428cc4f582f7b
gl89 <gloterena@gmail.com>
2016-10-10 23:15:03 -0400

    Added servlet files

---
+++ servlet/EchoServlet.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+ 
+public class EchoServlet extends HttpServlet {
+ 
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+               throws IOException, ServletException {
+      // Set the response message's MIME type
+      response.setContentType("text/html; charset=UTF-8");
+      // Allocate a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+ 
+      // Write the response message, in an HTML page
+      try {
+         out.println("<!DOCTYPE html>");
+         out.println("<html><head>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+         out.println("<title>Echo Servlet</title></head>");
+         out.println("<body><h2>You have enter</h2>");
+ 
+         String year = request.getParameter("year");
+         String team = request.getParameter("team");
+     
+        out.println("<p>Team: " + team + "</p>");
+		out.println("<p>Team: " + year + "</p>");
+       
+         // Hyperlink "BACK" to input page
+         out.println("<a href='form.html'>BACK</a>");
+ 
+         out.println("</body></html>");
+      } finally {
+         out.close();  // Always close the output writer
+      }
+   }
+ 
+   // Redirect POST request to GET request.
+   @Override
+   public void doPost(HttpServletRequest request, HttpServletResponse response)
+               throws IOException, ServletException {
+      doGet(request, response);
+   }
+ 
+   // Filter the string for special HTML characters to prevent
+   // command injection attack
+   private static String htmlFilter(String message) {
+      if (message == null) return null;
+      int len = message.length();
+      StringBuffer result = new StringBuffer(len + 20);
+      char aChar;
+ 
+      for (int i = 0; i < len; ++i) {
+         aChar = message.charAt(i);
+         switch (aChar) {
+             case '<': result.append("&lt;"); break;
+             case '>': result.append("&gt;"); break;
+             case '&': result.append("&amp;"); break;
+             case '"': result.append("&quot;"); break;
+             default: result.append(aChar);
+         }
+      }
+      return (result.toString());
+   }
+}
